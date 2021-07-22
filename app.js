7836b1fecbda52a0259fb47ee273395353e8506f
nicolas-moreira <nicolastoto2@gmail.com>
2021-07-22 17:13:54 +0200

    added config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const config = require('config');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = config.get('express.port');
 const path = require('path');
 const mongoose = require('./database');
 const server = app.listen(port, () => console.log('Server listening on port', port));
