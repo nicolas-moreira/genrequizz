75c0ee3e0b62aaf32ef87bdf980636c816a39ec6
nicolas-moreira <nicolastoto2@gmail.com>
2021-07-22 16:00:12 +0200

    moved file

---
+++ populateDB.js
@@ -0,0 +1,26 @@
+const mongoose = require('./database');
+const Name = require('./schemas/NameSchema');
+var fs = require('fs');
+
+
+var array = fs.readFileSync('./source/names.txt').toString().split("\n");
+
+function addToDatabase() {
+    for(i in array) {
+
+        if(/\S/.test(array[i]) && i <= 300){
+        
+            var prenom = array[i].split(" ")[0];
+            
+            if(prenom != null && prenom != ''){
+                console.log(` line ${i}`);
+                Name.create({firstName: prenom.toString()}).then((name) => console.log(name)).catch((err) => console.log(err));
+            }
+        
+        }
+    }
+}
+
+setTimeout(addToDatabase, 5000);
+
+
