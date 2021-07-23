d548bb5b6dc92696596d5710df6827dc3bb7a34f
nicolas-moreira <nicolastoto2@gmail.com>
2021-07-23 05:09:43 +0200

    refactor: readfile

--- populateDB.js
+++ populateDB.js
@@ -2,22 +2,23 @@ const mongoose = require('./database');
 const Name = require('./schemas/NameSchema');
 var fs = require('fs');
 
-
 var array = fs.readFileSync('./source/names.txt').toString().split("\n");
 
 function addToDatabase() {
-    for(i in array) {
+    fs.readFile("./source/names.txt", (err, data) => {
+        if (err) throw err;
 
-        if(/\S/.test(array[i]) && i < 300){
-        
-            var prenom = array[i].split(" ")[0];
-            
-            if(prenom != null && prenom != ''){
-                Name.create({firstName: prenom.toString()}).then((name) => console.log(name)).catch((err) => console.log(err));
+        data = data.toString().split("\n");
+        for(i in data) {
+
+            if(/\S/.test(data[i]) && i < 300){
+                var prenom = data[i].split(" ")[0];
+                if(prenom != null && prenom != ''){
+                    Name.create({firstName: prenom.toString()}).then((name) => console.log(name)).catch((err) => console.log(err));
+                }
             }
-        
         }
-    }
+      });
 }
 
 setTimeout(addToDatabase, 5000);
